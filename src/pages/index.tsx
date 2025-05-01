3681a94ff2f956b82be13b9e2b8738f2d81aaffd
MyrnaClyde <114366115+kauntdewn1@users.noreply.github.com>
2025-05-01 06:01:57 -0300

    fix: corrige importação do componente Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { Button } from '@/components/ui/button'
+import { Button } from '../components/Button'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
